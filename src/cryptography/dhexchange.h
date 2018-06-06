c6e90273351faec39bcae4ddf8aad392aa604aaa
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2018-06-06 17:06:50 +0200

    Add files for Diffie-Hellman exchange. Update CmakeLists.txt

---
+++ src/cryptography/dhexchange.h
@@ -0,0 +1,25 @@
+#ifndef DIFFIE_HELLMAN_EXCHANGE_ASM_H
+#define DIFFIE_HELLMAN_EXCHANGE_ASM_H
+#include <inttypes.h>
+#define DH_KEY_LENGTH	(16)
+
+typedef union _uint128_t {
+	struct {
+		uint64_t low;
+		uint64_t high;
+	};
+	unsigned char byte[DH_KEY_LENGTH];
+} uint128_t;
+
+typedef unsigned char DH_KEY[DH_KEY_LENGTH];
+
+void DH_generate_key_pair(DH_KEY public_key, DH_KEY private_key);
+void DH_generate_key_secret(DH_KEY secret_key, const DH_KEY my_private, const DH_KEY another_public);
+extern int test_asm(char a, char b, char c);
+extern int _u128_is_zero(const uint128_t dq);
+extern void _u128_make(uint128_t* dq, const DH_KEY key);
+extern int _u128_is_odd(const uint128_t dq);
+extern void _u128_lshift(uint128_t* dq);
+extern void _u128_rshift(uint128_t* dq);
+extern int _u128_compare(const uint128_t a, const uint128_t b);
+#endif
---
+++ src/cryptography/dhexchange.s
@@ -0,0 +1,104 @@
+.section .data
+####    base64_table: .ascii "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/\0"
+.section .text
+##############################################################
+.global test_asm
+.global _u128_is_zero
+.global _u128_make
+.global _u128_is_odd
+.global _u128_lshift
+.global _u128_rshift
+.global _u128_compare
+.global rand
+##############################################################
+# Calling convention parameter registers: %rdi, %rsi, %rdx, %rcx, %r8, %r9
+##############################################################
+.type test_asm, @function
+test_asm:
+	#int test(int a, int b, int c);
+	# %rdi - char* in, %rsi - char* out, %rdx len
+	call rand	
+	ret
+##############################################################
+.type _u128_is_zero, @function
+_u128_is_zero: #int(const uint128_t dq)
+	test %rdi, %rdi
+	jnz _u128_is_zero_false
+	test %rsi, %rsi
+	jnz _u128_is_zero_false
+	mov $1, %rax
+	ret
+_u128_is_zero_false:
+	mov $0, %rax
+	ret
+##############################################################
+.type _u128_make, @function
+_u128_make: #void(uint128_t* dq, const DH_KEY key)
+	mov (%rsi), %rax
+	mov %rax, (%rdi)
+	mov 8(%rsi), %rax
+	mov %rax, 8(%rdi)
+	ret
+##############################################################
+.type _u128_is_odd, @function
+_u128_is_odd: #int(const uint128_t dq)
+	bt $0, %rdi
+	jc _u128_is_odd_true
+	mov $0, %rax
+	ret
+_u128_is_odd_true:
+	mov $1, %rax
+	ret
+##############################################################
+.type _u128_lshift, @function 
+_u128_lshift: #void(uint128_t* dq) 
+	mov (%rdi), %rax
+	shr $63, %rax
+	shlq $1, 8(%rdi)
+	or %rax, 8(%rdi)
+	shlq $1, (%rdi)
+	ret
+##############################################################
+.type _u128_rshift, @function 
+_u128_rshift: #void(uint128_t* dq)
+	mov 8(%rdi), %rax
+	and $1, %rax
+	shl $63, %rax
+	shrq $1, 8(%rdi)
+	shrq $1, (%rdi)
+	or %rax, (%rdi)
+	ret
+##############################################################
+.type _u128_compare, @function
+_u128_compare: #int(const uint128_t a, const uint128_t b)
+
+	# a.low  -> %rdi
+	# a.high -> %rsi
+	# b.low  -> %rdx
+	# b.high -> %rcx
+
+	cmp %rcx, %rsi
+	jg _u128_compare_g
+	jl _u128_compare_l
+	cmp %rdx, %rdi
+	jg _u128_compare_g
+	jl _u128_compare_l
+	mov $0, %rax
+	ret
+_u128_compare_g:
+	mov $1, %rax
+	ret
+_u128_compare_l:
+	mov $-1, %rax
+	ret
+
+##############################################################
+##############################################################
+##############################################################
+##############################################################
+
+
+
+
+
+
