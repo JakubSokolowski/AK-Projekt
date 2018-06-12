cb0561f6db3794ab284ceba588e5e3dcf1de0d29
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2018-06-12 23:08:43 +0200

    Add function to print DH key

--- src/cryptography/dhexchange.h
+++ src/cryptography/dhexchange.h
@@ -1,6 +1,7 @@
 #ifndef DIFFIE_HELLMAN_EXCHANGE_ASM_H
 #define DIFFIE_HELLMAN_EXCHANGE_ASM_H
 #include <inttypes.h>
+#include <stdio.h>
 #define DH_KEY_LENGTH	(16)
 
 typedef union _uint128_t {
@@ -15,6 +16,16 @@ typedef unsigned char DH_KEY[DH_KEY_LENGTH];
 
 void DH_generate_key_pair(DH_KEY public_key, DH_KEY private_key);
 void DH_generate_key_secret(DH_KEY secret_key, const DH_KEY my_private, const DH_KEY another_public);
+void _print_key(const char* name, const DH_KEY key) {
+	int i;
+
+	printf("%s\t=\t", name);
+	for (i = DH_KEY_LENGTH-1; i>=0; i--) {
+		printf("%02x", key[i]);
+	}
+	printf("\n");
+}
+
 extern int test_asm(char a, char b, char c);
 extern int _u128_is_zero(const uint128_t dq);
 extern void _u128_make(uint128_t* dq, const DH_KEY key);
